565ef2c673cc7d3ddb51285bf4e66b1d019a8e18
Carlos Mello Jr <cmellojr@gmail.com>
2026-06-26 11:21:21 -0300

    add booking_app updates

--- booking_app/main.go
+++ booking_app/main.go
@@ -10,10 +10,17 @@ func main() {
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend.")
 
+	// bookings[1] = "Nicole"
+
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets int
+
+	// var bookings = [50]string  --> this is an array
+	var bookings = []string
+	// this is a slice
+	
 
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -22,6 +29,16 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+    // bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName + " " + lastName)
+
+	// fmt.Printf("The whole slice: %v\n", bookings)
+	// fmt.Printf("The first value: %v\n", bookings[0])
+	// fnt.Printf("Array type: %T\n", bookings)
+	// fmt.Printf("Slice length: %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+    fmt.Printf("These are all our bookings: %v\n", bookings)
 }
