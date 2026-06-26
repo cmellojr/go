e9ff7034ffaa124747b5b527ac94743cf6fb5581
Carlos Mello Jr <cmellojr@gmail.com>
2026-06-26 11:32:20 -0300

    add for loop

--- booking_app/main.go
+++ booking_app/main.go
@@ -3,42 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+    // var bookings = [50]string  --> this is an array
+	// bookings[1] = "Nicole"
+	bookings := []string{}
+	// this is a slice
+
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend.")
 
-	// bookings[1] = "Nicole"
+    for {
+	    var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	// var bookings = [50]string  --> this is an array
-	var bookings = []string
-	// this is a slice
-	
+		fmt.Println("Enter how many tickets you want: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		remainingTickets = remainingTickets - userTickets
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Println("Enter how many tickets you want: ")
-	fmt.Scan(&userTickets)
+		// fmt.Printf("The whole slice: %v\n", bookings)
+		// fmt.Printf("The first value: %v\n", bookings[0])
+		// fnt.Printf("Array type: %T\n", bookings)
+		// fmt.Printf("Slice length: %v\n", len(bookings))
 
-	remainingTickets = remainingTickets - userTickets
-    // bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName + " " + lastName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	// fmt.Printf("The whole slice: %v\n", bookings)
-	// fmt.Printf("The first value: %v\n", bookings[0])
-	// fnt.Printf("Array type: %T\n", bookings)
-	// fmt.Printf("Slice length: %v\n", len(bookings))
+		fmt.Printf("These are all our bookings: %v\n", bookings)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-    fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 }
